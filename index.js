413adb38542d767a41d8ed9244330abada3d8b4e
Harald Niesche <harald@niesche.de>
2022-01-13 19:26:01 +0100

    provide access to log contents

--- index.js
+++ index.js
@@ -16,9 +16,10 @@ beforeEach(function () {
   console._delayTasks = delayTasks;
   function delayedStream(orig) {
     const result = new Writable({
+      decodeStrings: false,
       write(chunk, encoding, callback) {
-        delayTasks.push(() => orig.write(chunk, encoding, () => {}));
-        queueMicrotask(callback);
+        delayTasks.push((s, cb) => (s??orig).write(chunk, encoding, cb));
+        callback && queueMicrotask(callback);
       }
     });
 
@@ -36,3 +37,61 @@ afterEach(function () {
   console = origConsole;
   //console.debug('afterEach (hook)');
 });
+
+async function logContains(what) {
+  //let re = (text instanceof RegExp) ? text : new RegExp(text);
+
+  const check = (what instanceof RegExp) 
+              ? (x) => (null != what.exec(x))
+              : (x) => x.includes(what);
+
+  const delayTasks = console._delayTasks;
+  if (delayTasks) {
+    let found = false;
+    const checkWriter = new Writable({
+      decodeStrings: false,
+      write(chunk, encoding, callback) {
+        const checkResult = check(chunk);
+        found = found || checkResult;
+        callback && queueMicrotask(callback);
+      }
+    });
+
+    const all = delayTasks.map((x) => donePromise(cb => x(checkWriter, cb)));
+    await Promise.all(all);
+    return found;
+  }
+  return false;
+}
+
+
+async function logContents() {
+  const delayTasks = console._delayTasks;
+  if (delayTasks) {
+    const contents = [];
+    const tmpWriter = new Writable({
+      decodeStrings: false,
+      write(chunk, encoding, callback) {
+        contents.push(chunk);
+        callback && queueMicrotask(callback);
+      }
+    });
+
+    const all = delayTasks.map((x) => donePromise(cb => x(tmpWriter, cb)));
+    await Promise.all(all);
+
+    return contents;
+  }
+  return undefined;
+}
+
+
+function donePromise(fun) {
+  const result = new Promise((res,_) => {
+    fun(res);
+  });
+  return result;
+}
+
+module.exports.logContains = logContains;
+module.exports.logContents = logContents;
--- test/test.js
+++ test/test.js
@@ -1,12 +1,26 @@
 
 const { assert } = require('chai');
 
-require('..'); // install our delayed logging code
+const { logContains, logContents } = require('..'); // install our delayed logging code
 
 describe('unit testing', function() {
   describe('with logging', function() {
     it('won\'t log visibly if successful', function() {
-      console.log('this should not be visible in test output');
+      console.log('this SHOULD NOT be visible in test output');
+    });
+    it('can verify logging in tests', async function() {
+      console.log('dummy log output 1');
+      console.warn('dummy log output 2');
+      console.warn('Just some log output, we\'ll check it was logged');
+
+      assert.isTrue(await logContains(/dummy log output \d/), ['a',...(await logContents())].join('#'));
+      assert.isTrue(await logContains('Just some log output, we\'ll check it was logged'), ['b',...(await logContents())].join('#'));
+    });
+    it('gives access to full logging contents', async function() {
+      const logMessage = "actual log contents will also include a newline after this";
+      console.log(logMessage);
+      const log = await logContents();
+      assert.strictEqual(log[0], logMessage+"\n");
     });
     it('will show all logging output for failed tests (and so this test must fail)', function() {
       console.log('dummy log');
