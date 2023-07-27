96298172197963573375a2a750d5bb31b4402353
Harald Niesche <harald@niesche.de>
2023-07-27 17:32:03 +0200

    fix export syntax

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,2 @@
-declare export function logContents():any[];
-declare export function logContains(what: RegExp|any):boolean;
+export declare function logContents():any[];
+export declare function logContains(what: RegExp|any):boolean;
